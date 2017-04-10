680d66f2fa1cb2bc1f199fddca22e0d00cde8fe2
Joshua Gross <joshua.gross@gmail.com>
2017-04-09 22:33:30 -0700

    Add plot1

---
+++ plot1.R
@@ -0,0 +1,16 @@
+# Experimentally verified that this is the correct offset in the file to get
+# data only for 1/2/2007 and 2/2/2007
+df  <- read.table("/Users/joshuagross/Downloads/household_power_consumption.txt", col.names=c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep=";", na.strings="?", nrows=2880, skip=66636)
+summary(df)
+names(df)
+
+png(filename = "plot1.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white", res = NA,
+    type = c("quartz"))
+
+hist(df$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col=c("red"))
+
+#plot(x,y)
+
+dev.off()
